be4cbcf7bed4fe991a9486979a9d2934be434f58
Govardhan R <govardhangajubali@gmail.com>
2021-08-09 16:43:19 +0530

    mail details checked

--- mailDetails.php
+++ mailDetails.php
@@ -7,8 +7,8 @@ $mail->SMTPDebug = SMTP::DEBUG_SERVER;                  //Enable verbose debug o
 $mail->isSMTP();                                        //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                   //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                               //Enable SMTP authentication
-$mail->Username   = 'biblio.dsce@gmail.com';            //SMTP username
-$mail->Password   = '1ds19csbiblio';                    //SMTP password
+$mail->Username   = 'dummy@example.com';            //SMTP username
+$mail->Password   = 'password';                    //SMTP password
 $mail->SMTPSecure = 'ssl';                              //Enable implicit TLS encryption
 $mail->Port       = 465;
 $mail->SMTPDebug = 0;                                //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
